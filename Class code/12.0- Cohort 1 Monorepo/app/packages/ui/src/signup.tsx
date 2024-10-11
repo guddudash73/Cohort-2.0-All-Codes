ba42bcc89a01bb9e8e9e905c87a537f28050d1c3
Guddu Dash <guddudash73@gmail.com>
2024-10-12 00:47:03 +0530

    cohort 1 monorepo

---
+++ Class code/12.0- Cohort 1 Monorepo/app/packages/ui/src/signup.tsx
@@ -0,0 +1,17 @@
+export const Signup = () => {
+  return (
+    <div
+      style={{
+        width: "100vh",
+        justifyContent: "center",
+        display: "flex",
+      }}
+    >
+      <div style={{ width: 400, border: "1px solid black" }}>
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="password" />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+};
